addf7ec724bfbdb5c2285bd370f3c863fb38c738
Jakub <gawlikjakub9@gmail.com>
2022-10-13 10:04:24 +0200

    login component cleanup

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,21 +1,32 @@
 import { useState } from "react";
-import { useAuthContext } from "../../hooks/useAuthContext";
 //styles
 
 const Login = () => {
-  const context = useAuthContext();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
 
   return (
     <div className="auth-container">
       <h2 className="auth-header">Login</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
-          <input type="email" placeholder="email" />
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label>
-          <input type="password" placeholder="password" />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <button className="signup-button">Login</button>
       </form>
