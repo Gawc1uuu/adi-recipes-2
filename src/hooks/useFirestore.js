482fe5e216acc22e8aa44d2e9f55e1af1dd7dcc4
Jakub <gawlikjakub9@gmail.com>
2022-10-16 12:34:03 +0200

    adding recipes

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,93 @@
+import { useReducer } from "react";
+import { db } from "../firebase/config";
+import {
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+  collection,
+} from "firebase/firestore";
+
+const initState = {
+  document: null,
+  isPending: false,
+  success: false,
+  error: null,
+};
+
+const firestoreReducer = (state, action) => {
+  switch (action.type) {
+    case "IS_PENDING":
+      return { ...state, isPending: true };
+    case "ADD_DOC":
+      return {
+        addedDocument: action.payload,
+        isPending: false,
+        success: true,
+        error: null,
+      };
+    case "DELETE_DOC":
+      return {
+        document: null,
+        isPending: false,
+        success: true,
+        error: null,
+      };
+    case "UPDATE_DOC":
+      return {
+        document: action.payload,
+        isPending: false,
+        success: true,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        document: null,
+        isPending: false,
+        success: false,
+        error: action.payload,
+      };
+    default:
+      return { ...state };
+  }
+};
+
+export const useFirestore = (col) => {
+  const [state, dispatch] = useReducer(firestoreReducer, initState);
+
+  const addDocument = async (document) => {
+    const docRef = collection(db, col);
+    dispatch({ type: "IS_PENDING" });
+    try {
+      const res = await addDoc(docRef, document);
+      console.log(res);
+      dispatch({ type: "ADD_DOC", payload: res });
+    } catch (err) {
+      dispatch({ type: "ERROR", payload: err.message });
+    }
+  };
+
+  const deleteDocument = async (id) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      await deleteDoc(doc(db, col, id));
+      dispatch({ type: "DELETED_DOC" });
+    } catch (err) {
+      dispatch({ type: "ERROR", payload: err.message });
+    }
+  };
+
+  const updateDocument = async (id, update) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      await updateDoc(doc(db, col, id), update);
+      dispatch({ type: "UPDATE_DOC" });
+    } catch (err) {
+      dispatch({ type: "ERROR", payload: err.message });
+    }
+  };
+
+  return { addDocument, deleteDocument, updateDocument, ...state };
+};
--- src/pages/createrecipe/CreateRecipe.js
+++ src/pages/createrecipe/CreateRecipe.js
@@ -1,12 +1,38 @@
 import "./CreateRecipe.css";
 
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useFirestore } from "../../hooks/useFirestore";
+import { serverTimestamp } from "firebase/firestore";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 const CreateRecipe = () => {
+  const navigate = useNavigate();
+  const { user } = useAuthContext();
+  const { addDocument } = useFirestore("recipes");
   const [name, setName] = useState("");
-  const [ingredients, setIngredients] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const [newIng, setNewIng] = useState("");
   const [method, setMethod] = useState("");
   const [prepTime, setPreptime] = useState();
+  const ingsInput = useRef(null);
+
+  const removeIng = (index) => {
+    setIngredients((prevState) => {
+      prevState.splice(index, 1);
+      return [...prevState];
+    });
+  };
+
+  const handleAdd = (e) => {
+    const ing = newIng.trim();
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prevState) => [...prevState, ing]);
+    }
+    setNewIng("");
+    ingsInput.current.focus();
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,9 +41,13 @@ const CreateRecipe = () => {
       ingredients,
       method,
       prepTime,
+      comments: [],
+      createdAt: serverTimestamp(),
+      createdBy: user.uid,
     };
 
-    console.log(recipe);
+    addDocument(recipe);
+    navigate("/");
   };
 
   return (
@@ -28,12 +58,30 @@ const CreateRecipe = () => {
       </label>
       <label>
         <span>Ingredients: </span>
-        <input
-          type="text"
-          required
-          onChange={(e) => setIngredients(e.target.value)}
-        />
+        <div className="ingredients-input">
+          <input
+            type="text"
+            onChange={(e) => setNewIng(e.target.value)}
+            value={newIng}
+            ref={ingsInput}
+          />
+          <button onClick={handleAdd} type="button" className="auth-button">
+            Add
+          </button>
+        </div>
       </label>
+      <p className="ings-list">
+        Current ingredients:
+        {ingredients.map((ing) => (
+          <em
+            key={ing}
+            onClick={() => removeIng(ingredients.indexOf(ing))}
+            className="ings-item"
+          >
+            {ing}
+          </em>
+        ))}
+      </p>
       <label>
         <span>Preparation method: </span>
         <textarea
