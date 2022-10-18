5c03267ac4420be58fb8a2f9375e5d27fa7e458e
Jakub <gawlikjakub9@gmail.com>
2022-10-18 14:41:52 +0200

    creating use document hook with firebase

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase/config";
+
+export const useDocument = (col, doc_id) => {
+  const [document, setDocument] = useState();
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    setError(null);
+    const docRef = doc(db, col, doc_id);
+    const unsub = onSnapshot(
+      docRef,
+      (doc) => {
+        setDocument(doc.data());
+      },
+      (err) => {
+        setError(err.message);
+      }
+    );
+
+    return () => unsub();
+  }, [col, doc_id]);
+
+  return { document, error };
+};
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -2,18 +2,36 @@
 import "./Recipe.css";
 import deleteIcon from "../../assets/delete-icon.svg";
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
 
 const Recipe = () => {
+  const { id } = useParams();
+  const { document, error } = useDocument("recipes", id);
+  console.log(document);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
   return (
     <div className="recipe">
-      <img src={deleteIcon} alt="edit icon" className="delete-icon" />
-      <div>
-        <h2>Recipe title</h2>
-        <em>created by: user</em>
+      <img src={deleteIcon} alt="edit icon" className="edit-icon" />
+      <div className="header">
+        <h2>{document.name}</h2>
+        <em>created by: {document.createdBy.userName}</em>
       </div>
-      <p>recipe ingredients</p>
-      <p>recipe method</p>
-      <p>cooking time</p>
+      <ul className="ingredients">
+        {document.ingredients.map((i) => (
+          <li key={i}>{i}</li>
+        ))}
+      </ul>
+      <p className="method">{document.method}</p>
+      <p className="cooking-time">{document.prepTime} minutes to make.</p>
     </div>
   );
 };
